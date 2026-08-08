d6de57b5cf24637715bcf781ba8d86e09b60c6cc
Ei Maung <eimg@fairwayweb.com>
2026-08-08 11:32:14 +0630

    project

--- project/_classes/Libs/Database/UsersTable.php
+++ project/_classes/Libs/Database/UsersTable.php
@@ -14,6 +14,16 @@ class UsersTable
         $this->db = $mysql->connect();
     }
 
+    public function all()
+    {
+        $result = $this->db->query(
+            "SELECT users.*, roles.name AS role
+            FROM users LEFT JOIN roles
+            ON users.role_id = roles.id"
+        );
+        return $result->fetchAll();
+    }
+
     public function find(string $email, string $password)
     {
         try {
@@ -46,5 +56,45 @@ class UsersTable
             echo $e->getMessage();
             exit();
         }
+    }
+
+    public function updatePhoto(string $id, string $photo)
+    {
+        $statement = $this->db->prepare("UPDATE users SET photo=:photo WHERE id=:id");
+        $statement->execute(['id' => $id, 'photo' => $photo]);
+
+        return $statement->rowCount();
+    }
+
+    public function changeRole(string $id, string $role_id)
+    {
+        $statement = $this->db->prepare("UPDATE users SET role_id=:role_id WHERE id=:id");
+        $statement->execute(['id' => $id, 'role_id' => $role_id]);
+
+        return $statement->rowCount();
+    }
+
+    public function suspend(string $id)
+    {
+        $statement = $this->db->prepare("UPDATE users SET suspended=1 WHERE id=:id");
+        $statement->execute(['id' => $id]);
+
+        return $statement->rowCount();
+    }
+
+    public function unsuspend(string $id)
+    {
+        $statement = $this->db->prepare("UPDATE users SET suspended=0 WHERE id=:id");
+        $statement->execute(['id' => $id]);
+
+        return $statement->rowCount();
+    }
+
+    public function delete(string $id)
+    {
+        $statement = $this->db->prepare("DELETE FROM users WHERE id=:id");
+        $statement->execute(['id' => $id]);
+
+        return $statement->rowCount();
     }
 }
--- project/profile.php
+++ project/profile.php
@@ -16,12 +16,25 @@
 <body>
     <div class="container" style="max-width: 600px">
         <h1 class="h3 mt-4 mb-3">Profile</h1>
+
+        <?php if($user->photo): ?>
+            <img src="_actions/photos/<?= $user->photo ?>" class="img-thumbnail" width="300">
+        <?php endif ?>
+
+        <form action="_actions/upload.php" method="post" 
+            class="input-group my-3" enctype="multipart/form-data">
+            <input type="file" name="photo" class="form-control">
+            <button class="btn btn-secondary">Upload</button>
+        </form>
+
         <ul class="list-group mb-3">
             <li class="list-group-item">Name: <?= $user->name ?></li>
             <li class="list-group-item">Email: <?= $user->email ?></li>
             <li class="list-group-item">Phone: <?= $user->phone ?></li>
             <li class="list-group-item">Address: <?= $user->address ?></li>
         </ul>
+
+        <a href="admin.php">Admin</a> |
 
         <a href="_actions/logout.php" class="text-danger">Logout</a>
     </div>
