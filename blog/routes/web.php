9153a2bb0db88ace7ee86fd768665b4218671d6c
Ei Maung <eimg@fairwayweb.com>
2026-08-15 10:52:28 +0630

    update

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
 
 Route::get('/', function () {
     return view('welcome');
